941acfc399070c17369ed58793f0a32e60a98080
sauravprakashgupta <iamsaurav.pr.gupta@hotmail.com>
2021-09-29 04:14:07 +0530

    first commit

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Learning Flask!'
+    return 'Learning Flask, first commit!'
 
 if __name__=='__main__':
     app.run(debug=True, port=3333)
